cf269ac7620eded28ff2e3c50c422882b69c995a
Kassahun Gedlu Degena <kassuahun@gmail.com>
2020-05-01 04:32:00 +0200

    Update pound.pp

--- manifests/pound.pp
+++ manifests/pound.pp
@@ -1,11 +1,13 @@
-package { 'pound':
-  ensure => present,
-  notify => Service['pound'],
-}
+class loadbalancer{
+  package { 'pound':
+    ensure => present,
+    notify => Service['pound'],
+  }
 
-service { 'pound':
-  ensure => running,
-  enable => true,
-  hasrestart => true,
-  require => Package['pound'],  
+  service { 'pound':
+    ensure => running,
+    enable => true,
+    hasrestart => true,
+    require => Package['pound'],  
+  }
 }
